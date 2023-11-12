6685946ef7b5282aa1520cb67cd984d3fd12b1e6
Minh Nguyen <mnguy025@odu.edu>
2023-11-12 00:01:26 -0500

    add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
 import "./App.css";
 
-import * as React from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import useConfig from "./components/useConfig";
-import logo from "./logo.svg";
+import MainPage from "./browser/pages/MainPage";
+import ErrorPage from "./browser/pages/ErrorPage";
+import { ReportPage } from "./browser/pages/ReportPage";
+import { TasksPage } from "./browser/pages/TasksPage";
+import { NewsPage } from "./browser/pages/NewsPage";
+import { LoginPage } from "./browser/pages/LoginPage";
+import { AboutPage } from "./browser/pages/AboutPage";
+import { Typography, Link, Container, Box, CssBaseline } from "@mui/material";
 
 /**
  * Our Web Application
  */
-export default function App() {
-  const config = useConfig();
+
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-    </div>
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        My Board
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function App() {
+  const { app } = useConfig();
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <CssBaseline />
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/report">
+            <ReportPage />
+          </Route>
+          <Route path="/tasks">
+            <TasksPage />
+          </Route>
+          <Route path="/news">
+            <NewsPage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
+
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: "auto",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light" ? theme.palette.grey[200] : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">Developed By Minh Nguyen</Typography>
+            <Copyright />
+          </Container>
+        </Box>
+      </Box>
+    </>
   );
 }
